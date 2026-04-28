30c57a179d67b99e59e50c88cf1713ad2bde7e67
Igor~ <igor@gmail.com>
2026-04-28 09:24:36 +0300

    28.04

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+
+  const clickHandler = () => {
+    if (text.trim() === "") return; // захист від пустого вводу
+    setTodo([...todo, { id: Math.random(), text }]);
+    setText(""); // очищаємо поле після додавання
+  };
+
   return (
     <div className="App">
       <Header />
-      <main>
-        <h1>Welcome to MyApp</h1>
-      </main>
+      <div>
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Введи завдання"
+        />
+      </div>
+      <button onClick={clickHandler}>add</button>
+      {todo.map((i) => (
+        <div key={i.id}>{i.text}</div>
+      ))}
     </div>
   );
 }
